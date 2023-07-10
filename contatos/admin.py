bc9a3cbe3af292de808a3ca306385796c0c11f18
Wanderson Rossetti <pastarossetti@gmail.com>
2023-07-10 11:14:01 -0300

    update

--- contatos/admin.py
+++ contatos/admin.py
@@ -4,4 +4,10 @@ from contatos import models
 
 @admin.register(models.Contato)
 class ContatoAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'email',
+    ordering = '-id',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 30
+    list_max_show_all = 200
+    list_editable = 'first_name', 'last_name',
+    list_display_links = 'id', 'phone',
