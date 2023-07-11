cc976e220610758387a389c5140cedb9b5f953d4
Wanderson Rossetti <pastarossetti@gmail.com>
2023-07-11 13:13:04 -0300

    update show list_display&list_editable

--- contatos/admin.py
+++ contatos/admin.py
@@ -4,12 +4,12 @@ from contatos import models
 
 @admin.register(models.Contato)
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone', 'email',
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'email', 'show',
     ordering = '-id',
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 30
     list_max_show_all = 200
-    list_editable = 'first_name', 'last_name',
+    list_editable = 'first_name', 'last_name', 'show',
     list_display_links = 'id', 'phone',
 
 
