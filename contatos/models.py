b170ea65339cf2b9afea4289f554f7bba29e6428
Wanderson Rossetti <pastarossetti@gmail.com>
2023-07-11 11:29:34 -0300

    update

--- contatos/models.py
+++ contatos/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -25,6 +26,11 @@ class Contato(models.Model):
         Category,
         on_delete=models.SET_NULL,
         blank=True, null=True
+    )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
     )
 
     def __str__(self) -> str:
