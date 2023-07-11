6fced02006eba23c29bc6297cab1d21073ab02e3
Wanderson Rossetti <pastarossetti@gmail.com>
2023-07-11 13:13:54 -0300

    creating contatos objects filters

--- contatos/views/contato_views.py
+++ contatos/views/contato_views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from contatos.models import Contato
 
 # Create your views here.
 
 
 def index(request):
+    contatos = Contato.objects.filter(show=True).order_by('-id')[:20]
+
+    context = {
+        'contatos': contatos,
+    }
+
     return render(
         request,
-        'contato/index.html'
+        'contato/index.html',
+        context,
     )
