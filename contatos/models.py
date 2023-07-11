872ff61fc9574d70106165343d59d2ce5d4384cf
Wanderson Rossetti <pastarossetti@gmail.com>
2023-07-11 10:29:23 -0300

    creating class category

--- contatos/models.py
+++ contatos/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Contato(models.Model):
@@ -11,6 +16,13 @@ class Contato(models.Model):
     email = models.EmailField(max_length=150, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
