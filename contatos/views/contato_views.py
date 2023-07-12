f2c930dd02d5a368f086701b5714eb722c12c48c
Wanderson Rossetti <pastarossetti@gmail.com>
2023-07-12 11:26:17 -0300

    creating pagination index & search

--- contatos/views/contato_views.py
+++ contatos/views/contato_views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from contatos.models import Contato
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 
 def index(request):
-    contatos = Contato.objects.filter(show=True).order_by('-id')[:20]
+    contatos = Contato.objects.filter(show=True).order_by('-id')
+
+    paginator = Paginator(contatos, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contatos': contatos,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
 
@@ -51,11 +56,16 @@ def search(request):
             Q(phone__icontains=search_value) |
             Q(email__icontains=search_value)
         )\
-        .order_by('-id')[:20]
+        .order_by('-id')
+    
+    paginator = Paginator(contatos, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contatos': contatos,
-        'site_title': 'Search - '
+        'page_obj': page_obj,
+        'site_title': 'Search - ',
+        'search_value': search_value,
     }
 
     return render(
