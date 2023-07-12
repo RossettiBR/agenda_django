457314305161f1d83e9bd4630db8fa83c2b059cd
Wanderson Rossetti <pastarossetti@gmail.com>
2023-07-12 11:36:42 -0300

    update crod contato

--- contatos/urls.py
+++ contatos/urls.py
@@ -4,7 +4,7 @@ from contatos import views
 app_name = 'contato'
 
 urlpatterns = [
-    path('<int:contact_id>', views.contato, name='contato'),  # type:ignore
+    path('contato/<int:contact_id>/detail/', views.contato, name='contato'),  # type:ignore
     path('search/', views.search, name='search'),  # type:ignore
     path('', views.index, name='index'),  # type:ignore
 ]
