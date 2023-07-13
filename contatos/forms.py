a319f3a1661e9108610c04f4b443e38c1098e60c
Wanderson Rossetti <pastarossetti@gmail.com>
2023-07-13 11:23:59 -0300

    creating first_name charfield widget

--- contatos/forms.py
+++ contatos/forms.py
@@ -1,14 +1,39 @@
-from django.shortcuts import render
-
 from contatos.models import Contato
 from django import forms
 from django.core.exceptions import ValidationError
 
 
 class ContatoForms(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'classe-a classe-b',
+                'placeholder': 'Aqui veio do init'
+            }
+        ),
+        label='Primeiro nome',
+        help_text='Texto para ajudar seu usuario'
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # self.fields['first_name'].widget.attrs.update({
+        #     'class': 'classe-a classe-b',
+        #     'placeholder': 'Aqui veio do init'
+        # })
+
     class Meta:
         model = Contato
         fields = 'first_name', 'last_name', 'phone',
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs={
+        #             'class': 'classe-a classe-b',
+        #             'placeholder': 'Escreva aqui!'
+        #         }
+        #     )
+        # }
 
     def clean(self):
         # cleaned_data = self.cleaned_data
