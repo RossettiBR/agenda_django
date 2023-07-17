bc06e3cd16fbb2f8f03346a92e8a7452960a79e1
Wanderson Rossetti <pastarossetti@gmail.com>
2023-07-17 09:48:59 -0300

    creating def RegisteForm

--- contatos/forms.py
+++ contatos/forms.py
@@ -1,7 +1,8 @@
 from contatos.models import Contato
 from django import forms
 from django.core.exceptions import ValidationError
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ContatoForms(forms.ModelForm):
     picture = forms.ImageField(
@@ -59,3 +60,35 @@ class ContatoForms(forms.ModelForm):
                 )
             )
         return first_name
+
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    email = forms.EmailField(
+        required=True,
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2',
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Já existe esse e-mail', code='invalid')
+            )
+
+        return email
